283cba08de66dab8494b5fa124b0dc4799895713
sushant swarup <116436039+sushantswarup@users.noreply.github.com>
2023-08-30 23:13:15 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ by Nayankumar Shetty © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ by Sushant Swarup © 2023 Studynotion</div>
         </div>
       </div>
     </div>
